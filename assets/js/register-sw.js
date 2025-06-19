8d146c3f1dc73066c9a6566887d5d95107713e05
Laxe4k <Laxe4k@hotmail.com>
2025-06-20 00:12:58 +0200

    feat: Update logo assets and enhance service worker registration messages

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -2,7 +2,7 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("/service-worker.js")
-      .then((reg) => console.log("✅ Service Worker enregistré", reg))
-      .catch((err) => console.error("❌ Échec Service Worker", err));
+      .then((reg) => console.log("✅ Service Worker enregistré avec succès :", reg.scope))
+      .catch((err) => console.error("❌ Échec de l'enregistrement du Service Worker :", err));
   });
 }
